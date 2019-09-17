edf505feeea52bc0f4101d4fdb5e757cf5e07da6
Yingqi Jing <350956009@qq.com>
2019-09-17 16:06:55 +0200

    edit compile.sh

--- compile.sh
+++ compile.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# Note: the id (1X3SyhXiKM0fMDTAVEW_gst-35O42TtV-L7OQGhHmrMs) should be changed according to your gdoc fiel id.
 gdrive export --force --mime text/plain 1X3SyhXiKM0fMDTAVEW_gst-35O42TtV-L7OQGhHmrMs
 
 gsed -e '0,/BEGINFILE/d' -e '/ENDFILE/,$d' length-harmony-locality.txt | sed -e '/________________/d' > main.txt
 
-rm length-harmony-locality.txt
+# this name also need to be changed according to the name of the .gdoc file.
+rm file.txt
 
 pandoc --template=simple.latex --pdf-engine=xelatex -F /usr/local/bin/glosses.py -F pandoc-citeproc -M secPrefix=Section -N main.txt -o mainr.tex 2> pandoc-errors.txt
 
